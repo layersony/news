0bc84f8c3a1909a71eb7a81a5db21458284a9d9f
layersony <sammaingi5@gmail.com>
2021-06-05 00:34:45 +0300

    add content config

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+  BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+  CATEGORY_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
+  NEWS_API = os.environ.get('NEWS_API')
+
+class ProdConfig(Config):
+  pass
+
+class DevConfig(Config):
+  DEBUG = True
+
+config_options = {
+  'development' : DevConfig,
+  'production': ProdConfig
+}
