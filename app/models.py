f80cfb6cf7397810c93f74c9e125106c7cca9d48
layersony <sammaingi5@gmail.com>
2021-06-05 08:31:25 +0300

    class for  source add

---
+++ app/models.py
@@ -0,0 +1,7 @@
+class source:
+  def __init__(self, id, name, description, category, url):
+    self.id = id
+    self.name = name
+    self.description = description
+    self.category = category
+    self.url = url
