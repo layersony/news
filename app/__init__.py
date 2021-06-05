5bda6272315268df579593d1d9d72dc31449c0cd
layersony <sammaingi5@gmail.com>
2021-06-05 10:50:15 +0300

    blueprint  and config initialized

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,17 @@ from config import config_options
 
 bootstrap = Bootstrap()
 
-def new_app(config_name):
+def news_app(config_name):
   app = Flask(__name__)
 
   app.config.from_object(config_options[config_name])
 
   bootstrap.init_app(app)
 
-  from .main import main as main_blueprint
-  app.register_blueprint(main_blueprint)
+  from .main import main
+  app.register_blueprint(main)
+
+  from .request import configure_request
+  configure_request(app)
 
   return app
