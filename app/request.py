b76fa51c47995c425a53977605e4d5de559d83fb
layersony <sammaingi5@gmail.com>
2021-06-05 09:18:24 +0300

    get news by catergory

---
+++ app/request.py
@@ -0,0 +1,28 @@
+import urllib.request, json
+# from app.models import Article, Source
+
+api_key = None
+base_url = None
+category_url = None
+
+def configure_request(app):
+  global base_url, category_url, api_key
+  api_key = app.config['NEWS_API']
+  base_url = app.config['BASE_URL']
+  category_url = app.config['CATEGORY_URL']
+
+def get_sources(sources):
+  get_source_url = category_url.format(sources, api_key)
+
+  # get_source_url = 'https://newsapi.org/v2/sources?category=business&apiKey=12ed8518c26c4277a1d828deca6db4ee'
+
+  with urllib.request.urlopen(get_source_url) as url:
+    get_catergory_data = url.read()
+    get_catergory_response = json.loads(get_catergory_data)
+
+    news_data = None
+
+    if get_catergory_response['sources']:
+      news_list = get_catergory_response['sources']
+      news_data = extractData(news_list)
+
