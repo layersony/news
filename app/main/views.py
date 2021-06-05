6be8e15662e64eacd54544afc08f80df7c190ade
layersony <sammaingi5@gmail.com>
2021-06-05 12:04:57 +0300

    display various catergories

--- app/main/views.py
+++ app/main/views.py
@@ -5,4 +5,11 @@ from ..request import get_sources
 @main.route('/')
 def index():
   general_source = get_sources('general')
-  return render_template('index.html', general = general_source)
+  business_source = get_sources('business')
+  ent_source = get_sources('entertainment')
+  health_source = get_sources('health')
+  science_source = get_sources('science')
+  sport_source = get_sources('sports')
+  tech_source = get_sources('technology')
+
+  return render_template('index.html', general = general_source, business= business_source, enterain = ent_source, health = health_source, science = science_source, sport = sport_source, tech = tech_source)
